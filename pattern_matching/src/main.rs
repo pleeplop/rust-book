d3d04c8995ccec37cd6861ae479e2e9e2ba99b6a
pleeplop <pleeplop@users.noreply.github.com>
2019-05-17 19:58:15 +0200

    pattern_matching part 2

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -37,10 +37,10 @@ fn main() {
     let point = (3, 5);
     print_coordinates(&point);
 
-    let x = 1;
+    let x = 3;
     match x {
-        1 => println!("one"),
-        2 => println!("two"),
+        1 | 2 => println!("one or two"),
+        3...5 => println!("three through five"),
         _ => println!("other"),
     }
 
@@ -52,6 +52,58 @@ fn main() {
         _ => println!("Default case, x = {:?}", x),
     };
     println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 'a';
+    match x {
+        'a'...'j' => println!("early ASCII letters"),
+        'k'...'z' => println!("late ASCII letters"),
+        _ => println!("something else"),
+    }
+
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    let msg = Message::ChangeColor(Color::Rgb(0, 160, 255));
+    match msg {
+        Message::Quit => println!("The quit variant has no data structure"),
+        Message::Move { x, y } => println!("Move in the x direction {} and in the y direction \
+        {}", x, y),
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(Color::Rgb(r, g, b)) =>
+            println!("Change the color to red {}, green {} and blue {}", r, g, b),
+        Message::ChangeColor(Color::Hsv(h, s, v)) =>
+            println!("Change the color to hue {}, saturation {} and value {}", h, s, v),
+    }
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
